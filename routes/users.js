caef76ae2f81a1f931ea05e795ae0d8b52aebc62
zhiqilin <zhiqilinn@gmail.com>
2022-05-09 19:57:22 -0400

    User Validators Done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,84 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
+
+const router = express.Router();
+
+router.get('/signup', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('create-user', {
+    title: 'Sign Up',
+    user,
+    csrfToken: req.csrfToken(),
+  });
 });
+
+const userValidators = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 50 })
+    .withMessage('First Name must not be more than 50 characters long'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Last Name')
+    .isLength({ max: 50 })
+    .withMessage('Last Name must not be more than 50 characters long'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for username')
+    .isLength({ max: 50 })
+    .withMessage('Last Name must not be more than 50 characters long'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email'),
+  check('hashPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+
+];
+
+router.post('/signup', csrfProtection, userValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      email,
+      firstName,
+      lastName,
+      username,
+      hashPassword,
+    } = req.body;
+
+    const user = db.User.build({
+      email,
+      firstName,
+      lastName,
+      username,
+    });
+
+    // ---------------------------------------
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await user.save();
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('user-register', {
+        title: 'Register',
+        user,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
 
 module.exports = router;
