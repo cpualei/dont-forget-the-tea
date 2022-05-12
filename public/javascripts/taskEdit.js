4816a72444b6cddf82ce16bae60e00cb5db01e67
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-05-12 14:50:42 -0700

    created a hidden class for editting task form. added an event listener to edit btn, when clicked, form appears. when cancelled, form is hidden

--- public/javascripts/taskEdit.js
+++ public/javascripts/taskEdit.js
@@ -1,31 +1,64 @@
-document.addEventListener("DOMContentLoaded", async () => {
+
     //grabbing all the btn-edit-subtask buttons
 
-const editBtnsArr = document.querySelectorAll('.btn-edit-task')
+console.log('SCRIPT FILE IS RUNNING')
+
+const editBtnsArr = document.querySelectorAll('.btn-edit-task');
 
 //iterate through an arr of buttons
-for (let i = 0; i < editBtnsArr.length; i++) {
-    const btn = editBtns[i];
+editBtnsArr.forEach(btn => {
+    btn.addEventListener('click', async(e) => {
+        e.preventDefault();
+        const taskId = e.target.id.split('-')[2];
+        console.log(taskId, "THIS IS THE TASK ID IN EDIT------")
 
-    //add eventlistener for each button to have it editted 
-    btn.addEventListener('click', (e) => {
-        const taskId = e.target.id.split('-')[2]
-            const res = await fetch(`/tasks/${taskId}`, {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    content
-                })
-            });
-        const data = await res.json()
-                if (data.message === 'Success') {
-                    const contentEle = document.getElementById(`${taskId}-content`)
-                    console.log("CONTENT ELEMENT ",contentEle)
-                    contentEle.innerHTML = data.body.content
-                } else {
+        const editForm = document.querySelector(`#edit-task-form-${taskId}`);
+        editForm.classList.remove('hidden');
 
-                };
+        // const res = await fetch(`/tasks/${taskId}`, {
+        //     method: 'PUT',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({
+        //         content
+        //     })
+        // });
+        // const data = await res.json()
+        //     if (data.message === 'Success') {
+        //         const contentEle = document.getElementById(`${taskId}-content`)
+        //         console.log("CONTENT ELEMENT ",contentEle)
+        //         contentEle.innerHTML = data.task.content
+        //     } else {
+
+        //     };
     });
-};
+});
 
+
+const cancelBtnsArr = document.querySelectorAll('.btn-cancel-task');
+console.log(cancelBtnsArr)
+cancelBtnsArr.forEach(btn => {
+    btn.addEventListener('click', async(e) => {
+        e.preventDefault();
+        const taskId = e.target.id.split('-')[3];
+        console.log(taskId, "THIS IS THE TASK ID IN CANCEL------")
+        const editForm = document.querySelector(`#edit-task-form-${taskId}`);
+        console.log(editForm)
+        editForm.classList.add('hidden');
+
+        // const res = await fetch(`/tasks/${taskId}`, {
+        //     method: 'PUT',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({
+        //         content
+        //     })
+        // });
+        // const data = await res.json()
+        //     if (data.message === 'Success') {
+        //         const contentEle = document.getElementById(`${taskId}-content`)
+        //         console.log("CONTENT ELEMENT ",contentEle)
+        //         contentEle.innerHTML = data.task.content
+        //     } else {
+
+        //     };
+    });
 });
