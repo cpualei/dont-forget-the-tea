3a11a771c59c4d2e62b7c0e85f1598529d0940d4
Weiqi Mei <weiqimei@ucsb.edu>
2022-05-15 15:19:47 -0700

    login route

--- routes/users.js
+++ routes/users.js
@@ -187,7 +187,7 @@ router.post('/login', csrfProtection, userLoginValidator, asyncHandler(async (re
       }
     }
   } else {
-    console.log("IN THE ELSE BLOCK OF VALIDATION ERRORS.ISEMPTY, DID NOT PASS userLoginValidator" )
+    console.log("IN THE ELSE BLOCK OF VALIDATION ERRORS.ISEMPTY, DID NOT PASS userLoginValidator")
     errors = validatorErrors.array().map((error) => error.msg);
     res.render('login', { //re-render login page
       csrfToken: req.csrfToken(),
